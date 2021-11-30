426503b3fe3e5dacd030e5cfd953b560aa86d428
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-11-30 20:11:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
+int n;
 int main(){
-  return 0;
+    cin>>n;
+    if(ric(2,n)){
+        cout<<"numero primo"<<endl;
+    }else{
+        cout<<"numero non primo"<<endl;
+    }
+    return 0;
 }
